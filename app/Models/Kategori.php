91021446e765d23fffa9b62cc05a6bb6f6f51d68
root <root@debian.top>
2024-06-18 23:34:07 -0400

    first commit

---
+++ app/Models/Kategori.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Kategori extends Model
+{
+    use HasFactory;
+
+    protected $table = 'kategori';
+
+    protected $fillable = ['deskripsi', 'kategori'];
+
+    public function barang()
+    {
+        return $this->hasMany(Barang::class);
+    }
+
+    public static function search($query)
+    {
+        return self::where('deskripsi', 'LIKE', "%{$query}%")
+            ->orWhere('kategori', 'LIKE', "%{$query}%")
+            ->get();
+    }
+
+    public static function getKategoriAll()
+    {
+        return DB::table('kategori')
+            ->select('kategori.id', 'deskripsi', DB::raw('ketKategori(kategori) as ketkategori'), 'kategori') // Add 'kategori' here
+            ->get();
+    }
+}
